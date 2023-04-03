532ff64dcd2a5b4343ef5cc5b6d2672645877175
Давид <tituradavid@gmail.com>
2023-04-03 22:17:58 +0300

    уже рабочая програмка

--- back_end.py
+++ back_end.py
@@ -1,2 +1,8 @@
-def shet():
-    pass
+def shet(arg):
+    time_in_sec = 5900000000 / arg
+    time_in_min = time_in_sec / 60
+    time_in_ocl = time_in_min / 60
+    time_in_day = time_in_ocl / 24
+    time_in_month = time_in_day / 30
+    time_in_year = time_in_month / 12
+    return round(time_in_year)
--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import customtkinter
 
+
+def work():
+    res = int(entr.get())
+    import back_end
+    btt = back_end.shet(res)
+    lab_2.configure(text=f"Время пути до плутона {btt} лет")
+
+
 win = customtkinter.CTk()
 win.title("Эксперимент")
 win.geometry("500x500")
 
-lab = customtkinter.CTkLabel(win, text="Впиши скорость в киллометрах \nв секунду и узнай время пути до плутона",
+lab = customtkinter.CTkLabel(win, text="Впиши скорость в киллометрах в секунду \nи узнай время пути до плутона",
                              font=("Arial Bold", 24))
 lab.place(relx=0.02, rely=0.10)
 
-entr = customtkinter.CTkEntry(win, width=200)
+lab_2 = customtkinter.CTkLabel(win, text="", font=("Arial Bold", 15))
+lab_2.place(relx=0.25, rely=0.60)
+
+entr = customtkinter.CTkEntry(win, width=200, height=35)
 entr.place(relx=0.30, rely=0.30)
 
-btn = customtkinter.CTkButton(win, text="Обработать")
-btn.place(relx=0.35, rely=0.40)
+btn = customtkinter.CTkButton(win, text="Обработать", command=work, font=("Arial Bold", 25))
+btn.place(relx=0.34, rely=0.40)
 
 
 win.mainloop()
